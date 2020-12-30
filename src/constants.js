910863ab9cfe2fb5b929a83aac37f44e0e7bd9f3
abdellatif T idrissi <buyukpatron@gmail.com>
2020-12-30 13:15:25 +0100

    commit inadora digital agency website

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  bgBtn: "#00e06d",
+  txtColor: "#300753",
+  bgColor: "#300753",
+  secTxtColor: "#00e06d",
+  primary: "white",
+};
+export const SIZES = [
+  8,
+  16,
+  24,
+  32,
+  /* And so on */
+];
